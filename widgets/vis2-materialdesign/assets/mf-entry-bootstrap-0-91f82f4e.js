c5fa60c36950839c869c002e2b5b2a5af28f7f06
typhosj <mrennecke@gmx.de>
2026-07-24 11:38:00 +0200

    chore(m3): rebuild widget bundle for Phase 6
    Regenerated widgets/ and admin/ build output for the Top App Bar M3 changes.

--- widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-91f82f4e.js
+++ widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-91f82f4e.js
@@ -32,4 +32,4 @@ const __mfImport = (src) =>
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-CXX-mUMF.js"));
+})().then(() => __mfImport("./index-DT8yGjlQ.js"));
