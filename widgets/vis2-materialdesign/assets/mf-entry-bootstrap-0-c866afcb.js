4434071e584b0e60a5fcf507e38be29e2e16b919
typhosj <mrennecke@gmx.de>
2026-07-24 11:32:26 +0200

    chore(m3): rebuild widget bundle for Phase 5
    Regenerated widgets/ and admin/ build output for the Progress/Alerts/Dialog M3 changes.

--- widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-c866afcb.js
+++ widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-c866afcb.js
@@ -32,4 +32,4 @@ const __mfImport = (src) =>
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-CEh3Y5OT.js"));
+})().then(() => __mfImport("./index-CXX-mUMF.js"));
